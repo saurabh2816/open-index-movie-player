917449be1ead221c103351bc15c4b21ffc253d19
saurabh2816 <saurabh2816#>
2022-04-16 14:36:23 -0700

    works for olders video player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatVideoModule } from 'mat-video';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatVideoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
